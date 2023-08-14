a8855f28702e7c5075aee09afea565d919cb1091
PrimalPimmy <Prashant20.pm@gmail.com>
2023-08-14 22:02:39 +0530

    Test framework for k8tls
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

---
+++ tests/kubetls-test.go
@@ -0,0 +1,79 @@
+package test
+
+import (
+	"encoding/csv"
+	"os"
+
+	. "github.com/kubearmor/KubeArmor/tests/util"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = BeforeSuite(func() {
+	// err := K8sApply([]string{"res/test-k8tls.yaml"})
+	// Expect(err).To(BeNil())
+	// install wordpress-mysql app
+	err := K8sApply([]string{"res/test-k8tls.yaml", "res/wordpress-mysql-dep.yaml"})
+	Expect(err).To(BeNil())
+
+})
+
+var _ = AfterSuite(func() {
+
+	err := K8sDelete([]string{"res/test-k8tls.yaml", "res/wordpress-mysql-dep.yaml"})
+	Expect(err).To(BeNil())
+
+})
+
+func matchCSV(file1 string, file2 string) {
+	// Read the first CSV file
+	file1Data, err := os.Open(file1)
+	Expect(err).NotTo(HaveOccurred())
+	defer file1Data.Close()
+
+	reader1 := csv.NewReader(file1Data)
+	file1Records, err := reader1.ReadAll()
+	Expect(err).NotTo(HaveOccurred())
+
+	// Read the second CSV file
+	file2Data, err := os.Open(file2)
+	Expect(err).NotTo(HaveOccurred())
+	defer file2Data.Close()
+
+	reader2 := csv.NewReader(file2Data)
+	file2Records, err := reader2.ReadAll()
+	Expect(err).NotTo(HaveOccurred())
+
+	// Compare headers
+	Expect(len(file1Records[0])).To(Equal(len(file2Records[0])))
+
+	for i := 0; i < len(file1Records[0]); i++ {
+		Expect(file1Records[0][i]).To(Equal(file2Records[0][i]))
+	}
+
+	// Compare rows
+	Expect(len(file1Records)).To(Equal(len(file2Records)))
+
+	for i := 0; i < len(file1Records); i++ {
+		for j := 0; j < len(file1Records[i]); j++ {
+			Expect(file1Records[i][j]).To(Equal(file2Records[i][j]))
+		}
+	}
+}
+
+var _ = Describe("CSV Matching", func() {
+
+	It("should match the summary output files", func() {
+
+		file1 := "/tmp/summary.csv"
+		file2 := "summary-test.csv"
+		matchCSV(file1, file2)
+	})
+
+	It("should match the table output files", func() {
+		file1 := "/tmp/out.csv"
+		file2 := "table-test.csv"
+		matchCSV(file1, file2)
+	})
+})
