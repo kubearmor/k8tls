8f9c1a22a97421f84dfca81bac851d5b8fd19f58
PrimalPimmy <Prashant20.pm@gmail.com>
2023-08-22 14:46:22 +0530

    Updated install script
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

--- .github/workflows/scripts/install_k3s.sh
+++ .github/workflows/scripts/install_k3s.sh
@@ -5,5 +5,5 @@ mkdir -p ~/.kube && cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
 until kubectl wait --for=condition=ready --timeout=15m -n kube-system pod -l k8s-app=metrics-server
 do
  [[ $? != 0 ]] && echo "Checking for metrics-server"
- sleep 10
+ sleep 5
 done
